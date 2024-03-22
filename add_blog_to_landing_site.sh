f100be0bb1984774e873237e4452def082e9515d
x4ndri <aqader3@gatech.edu>
2024-03-22 00:09:01 -0400

    degub deploy

--- add_blog_to_landing_site.sh
+++ add_blog_to_landing_site.sh
@@ -1,11 +1,11 @@
 rm -r blog
-cd static/blog
+cd static
 hugo
-cd ../..
-cp -r static/blog/public ./blog
+cd ..
+cp -r static/public ./blog
 
 sleep 1
 
 git add .
 git commit -m "degub deploy"
-git push origin dev
+git push origin main
